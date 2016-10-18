8a26cf12bf2e0b564a713ca16191e578dcb5589a
Tannyzz <alejandro.plancarte@estudiante.uacm.edu.mx>
2016-10-17 23:43:51 -0500

    Webv1.0

---
+++ js/js-naveli.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+	 $(".button-collapse").sideNav();
+	 $('.parallax').parallax();
+	 $('.modal-trigger').leanModal();
+	 $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
