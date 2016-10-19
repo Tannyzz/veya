bddda18a60b41b8cd6d9da5d28487e1b4f098f84
Tannyzz <alejandro.plancarte@estudiante.uacm.edu.mx>
2016-10-19 16:01:55 -0500

    Cambio en la extension del archivo para la creacion del formulario en PHP

---
+++ contacto.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contacto - Verón Esquivel & Asociados</title>
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="css/style-naveli.css">
+    <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <meta name="theme-color" content="#e0e0e0">
+    <meta charset="utf-8">
+</head>
+<body>
+
+	<div class="navbar-fixed">
+	    <nav class="white z-depth-2 bar-nav">
+		    <div class="nav-wrapper ">
+		      <a href="index.html" class="brand-logo"><img class="responsive-img logo" src="images/logo.png"></a>
+		      <a href="#" data-activates="mobile-demo" class="button-collapse indigo-text"><i class="material-icons">menu</i></a>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down list-nav">
+		      <li><a href="index.html" class="indigo-text">Inicio</a></li>
+		        <li><a href="servicios.html" class="indigo-text">Servicios</a></li>
+		        <li><a href="noticias.html" class="indigo-text">Noticias</a></li>
+		        <li><a href="integrate.html" class="indigo-text">Intégrate</a></li>		        
+		        <li><a href="#!" class="indigo-text">Contacto</a></li>
+		      </ul>
+		      <ul class="side-nav" id="mobile-demo">
+		      <li class="logo-mobil center-align indigo-text z-depth-1">Verón Esquivel & Asociados</li>
+		      <li><a href="index.html" class="indigo-text">Inicio</a></li>
+		        <li><a href="servicios.html" class="indigo-text">Servicios</a></li>
+		        <li><a href="noticias.html" class="indigo-text">Noticias</a></li>
+		        <li><a href="integrate.html" class="indigo-text">Intégrate</a></li>
+		        <li><a href="#!" class="indigo-text">Contacto</a></li>
+		      </ul>
+		    </div>
+		</nav>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col s m l"><p´</p></div>
+			<div class="col s m l"><p>´</p></div>
+		</div>
+	</div>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m5 l5"><img src="images/logo.png" class="responsive-img"></div>
+			<div class="col s12 m7 l7 right-align valign-wrapper"><h4 style="font-style: italic;" class=" grey-text lighten-2">Sumamos nuestra experiencia a tu desarrollo</h4></div>
+		</div>
+	</div>
+	<div class="divider indigo container"></div>
+
+	<div class="section white">
+	    <div class="row container">
+	      <h2 class="header indigo-text">Contáctanos</h2>
+	      <p class="grey-text text-darken-3 lighten-3">Contáctanos para más información sobre nuestros productos y servicios:</p>
+	      <div class="row">
+		    <form class="col s12" method="POST">
+		      <div class="row">
+		        <div class="input-field col s12 m6 l6">
+		          <i class="material-icons prefix indigo-text">account_circle</i>
+		          <input required id="icon_prefix" name="name" type="text" class="validate">
+		          <label for="icon_prefix">Nombre completo</label>
+		        </div>
+		        <div class="input-field col s12 m6 l6">
+		          <i class="material-icons prefix indigo-text">phone</i>
+		          <input required id="icon_telephone" name="tel" type="tel" class="validate">
+		          <label for="icon_telephone">Teléfono</label>
+		        </div>
+		      </div>
+		      <div class="row">
+				<div class="input-field col s12 m6 l6">
+				<i class="material-icons prefix indigo-text">email</i>
+		          <input required id="email" name="mail" type="email" class="validate">
+		          <label for="email">Email</label>
+		        </div>
+		        <div class="input-field col s12 m6 l6">
+		          <i class="material-icons prefix indigo-text">subject</i>
+		          <input required id="icon_prefix" name="subject" type="text" class="validate">
+		          <label for="icon_prefix">Asunto</label>
+		        </div>
+		        </div>
+		        <div class="row">
+		        	<div class="input-field col s12">
+		        	<i class="material-icons prefix indigo-text">mode_edit</i>
+			          <textarea required id="textarea1" name="coment" class="materialize-textarea"></textarea>
+			          <label for="textarea1">Comentarios</label>
+			        </div>
+		        </div>
+		        <div class="right-align">
+			        <button class="btn waves-effect waves-light indigo" type="submit" name="send">Enviar
+					    <i class="material-icons right">send</i></button>
+				</div>
+		    </form>
+		  </div>
+	    </div>
+	</div>
+	<?php 
+	if(isset($_POST['send'])){
+
+		$msj = "<img src=\"https://tannyzz.github.io/veya/images/logo.png\"><br><br>".
+				"<h1 style=\"color: #3f51b5;\">CONTACTO</h1>".
+				"<h3 style=\"color: #3f51b5;\">Correo proveniente del sitio web</h3><br>".
+				"*NOMBRE: <b>".$_POST['name']."</b><br><br>".
+				"*TELÉFONO: <b>".$_POST['tel']."</b><br><br>".
+				"*EMAIL DEL INTERESADO: <b>".$_POST['mail']."</b><br><br>".
+				"*ASUNTO: <b>".$_POST['subject']."</b><br><br>".
+				"*COMENTARIOS: <b>".$_POST['coment']."</b>";
+
+		$headers .= "MIME-Version: 1.0\r\n"; 
+		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+		$headers .= "From: Contacto VE&A Sitio WEB  <info@veronesquivel.com.mx>\r\n";
+		$headers .= 'Cc: nominas@veronesquivel.com.mx' . "\r\n";		
+		$bool = mail("info@veronesquivel.com.mx",$_POST['subject'],$msj, $headers);
+
+		if($bool){
+				echo "<div class=\"center-align\"><h4 class=\"green-text lighten-1\">Su correo se fue enviado existosamente</h4></div>";
+			}else{
+				echo "<div class=\"center-align\"><h4 class=\"red-text lighten-1\">Hubo un error al enviar el correo. Intenta de nuevo.</h4></div>";
+			}
+		
+		
+	}
+ ?>
+
+	<footer class="page-footer grey lighten-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class=" indigo-text">Verón Esquivel & Asociados</h5>
+                <p class=" grey-text darken-5 flow-text">Solicita a través de los medios de contacto de Verón Esquivel asesoramiento y cotización sobre los servicios requeridos. Si deseas ver nuestra ubicación exacta da click <a class="indigo-text" target="_blank" href="https://www.google.com/maps/place/Calle+Isla+San+Juan+de+Ul%C3%BAa+87,+Hab+Prado+Vallejo,+54170+Tlalnepantla,+M%C3%A9x.,+M%C3%A9xico/@19.5095723,-99.1648685,3a,75y,264h,90t/data=!3m7!1e1!3m5!1s4pG2r-Ssl2Rp47RtmQiVoQ!2e0!6s%2F%2Fgeo3.ggpht.com%2Fcbk%3Fpanoid%3D4pG2r-Ssl2Rp47RtmQiVoQ%26output%3Dthumbnail%26cb_client%3Dsearch.TACTILE.gps%26thumb%3D2%26w%3D86%26h%3D86%26yaw%3D264.62424%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656!4m5!3m4!1s0x85d1f83c34a61a69:0x2f5becb68ecd9a1a!8m2!3d19.5095538!4d-99.1649434!6m1!1e1?hl=es-MX">aquí.</a></p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3760.7461625625756!2d-99.16713208545887!3d19.509553786840648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f83c34a61a69%3A0x2f5becb68ecd9a1a!2sCalle+Isla+San+Juan+de+Ul%C3%BAa+87%2C+Hab+Prado+Vallejo%2C+54170+Tlalnepantla%2C+M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1476738504527" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="indigo-text darken-5">Contáctanos</h5>
+                <ul>
+                	<h6 class="indigo-text darken-5">Telefónos</h6>
+                  <li class="grey-text darken-5"><i class="material-icons">phone</i>&nbsp;&nbsp;53897029</li>
+                  <li class="grey-text darken-5"><i class="material-icons">phone</i>&nbsp;&nbsp;50391689</li>
+                  <li class="grey-text darken-5"><i class="material-icons">phone</i>&nbsp;&nbsp;53676647</li>
+                  <li class="grey-text darken-5"><i class="material-icons">phone</i>&nbsp;&nbsp;53690265</li><br>
+                   <h6 class="indigo-text darken-5">Email</h6>
+                  <li class="grey-text darken-5"><i class="material-icons">email</i>&nbsp;&nbsp;nominas@veronesquivel.com.mx</li><br>
+                  <li><div class="row">
+                  		<div class="col s12">
+                  				<a href="https://www.facebook.com/profile.php?id=100010479312399&fref=ts" target="_blank"><img width="50px" height="50px" src="images/facebook_f.png" class="responsive-img">
+                  				<a href="https://twitter.com/VeronEsquivel" target="_blank"><img width="50px" height="50px" src="images/twitter_f.png" class="responsive-img">
+                  		</div>
+                  	  </div>
+                  </li>
+                  <h6 class="indigo-text darken-5">Dirección</h6>
+                  <li class="grey-text darken-5"><i class="material-icons">place</i>&nbsp;&nbsp;Calle isla San Juan de Ulúa No. 87, Col. Prado Vallejo, CP. 54170, Tlalnepantla, Edo de Mex.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container grey-text darken-5">
+            © 2016 VE&A. Todos los derechos reservados.
+            </div>
+          </div>
+        </footer>
+	
+
+
+
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/js-naveli.js"></script>
+
+</body>
+</html>
