bdf72ff3377f0e6185d1cf26b14550b45ebfb152
Tannyzz <alejandro.plancarte@estudiante.uacm.edu.mx>
2016-10-25 13:51:39 -0500

    Cambio del hover a true

--- js/js-naveli.js
+++ js/js-naveli.js
@@ -2,6 +2,8 @@ $( document ).ready(function(){
 	 $(".button-collapse").sideNav();
 	 $('.parallax').parallax();
 	 $('.modal-trigger').leanModal();
+	 $('.scrollspy').scrollSpy();
+	 $(".dropdown-button").dropdown();
 	 $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
